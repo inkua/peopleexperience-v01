97d24622fd007a9a40f1eaac558877cd47367090
Tiago <tiago.zdo@gmail.com>
2024-08-15 19:36:20 -0300

    feat: create a new user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,22 @@
 import { prisma } from "@/libs/prisma";
 
+export const createUser = async (data) => {
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                teamId: data.teamId,
+                typeOfUser: data.typeOfUser || "USER",
+            },
+        });
+        return newUser;
+    } catch (error) {
+        throw new Error("Failed to create user");
+    }
+};
+
 export const getUserById = async (id) => {
     const user = await prisma.user.findUnique({
         where: {
@@ -35,4 +52,4 @@ export const getUserById = async (id) => {
     });
 
     return user;
-}
+};
