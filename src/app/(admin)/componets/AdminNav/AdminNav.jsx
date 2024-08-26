a09831b5b5beda7ebc96d0ec30ae5ff7d6d414c8
manuel14mds <manuel14mds@gmail.com>
2024-08-26 17:31:04 -0500

    update: navigation list - current links view

--- src/app/(admin)/componets/AdminNav/AdminNav.jsx
+++ src/app/(admin)/componets/AdminNav/AdminNav.jsx
@@ -1,3 +1,7 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
@@ -7,9 +11,8 @@ const user = {
     imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
+
 const navigation = [
-
-
     { name: 'Dashboard', href: '/admin', current: true },
     { name: 'Participantes', href: '/admin/participants', current: false },
     { name: 'Equipos', href: '/admin/teams', current: false },
@@ -17,8 +20,6 @@ const navigation = [
     { name: 'Reports', href: '#', current: false },
 ]
 const userNavigation = [
-
-
     { name: 'Your Profile', href: '#' },
     { name: 'Settings', href: '#' },
     { name: 'Sign out', href: '#' },
@@ -27,10 +28,14 @@ const userNavigation = [
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
-
-
+const isCurrentPath=(current, href)=>{
+    if(current==href) return true
+    return false
+}
 
 function AdminNav() {
+    const pathname = usePathname()
+
     return (
         <Disclosure as="nav" className="bg-gray-800">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -49,9 +54,9 @@ function AdminNav() {
                                     <a
                                         key={item.name}
                                         href={item.href}
-                                        aria-current={item.current ? 'page' : undefined}
+                                        aria-current={isCurrentPath(pathname, item.href) ? 'page' : undefined}
                                         className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            isCurrentPath(pathname, item.href) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                             'rounded-md px-3 py-2 text-sm font-medium',
                                         )}
                                     >
@@ -118,9 +123,9 @@ function AdminNav() {
                             key={item.name}
                             as="a"
                             href={item.href}
-                            aria-current={item.current ? 'page' : undefined}
+                            aria-current={isCurrentPath(pathname, item.href) ? 'page' : undefined}
                             className={classNames(
-                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                isCurrentPath(pathname, item.href) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                 'block rounded-md px-3 py-2 text-base font-medium',
                             )}
                         >
