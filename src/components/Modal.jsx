31af3b7ed4fae774e8b5975a82f117f2a9df07d6
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2024-09-17 19:00:49 -0300

    creacion del componente form y correcciones en los archivos page de participants, modal y addbtn

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+'use client';
+
+import { useState, useCallback } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 
-export default function Modal({ title, text, open, setOpen }) {
+export default function Modal({ children, open, setOpen, showButtons }) {
 
     const handleClose = useCallback(() => setOpen(false), [setOpen]);
 
@@ -11,28 +13,14 @@ export default function Modal({ title, text, open, setOpen }) {
         transition
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
       />
-
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
             className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <div className="sm:flex sm:items-start">
-                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                    {title}
-                  </DialogTitle>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      {text}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            {children}
+            { showButtons ? (<div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
                 onClick={handleClose}
@@ -48,7 +36,7 @@ export default function Modal({ title, text, open, setOpen }) {
               >
                 Cancelar
               </button>
-            </div>
+            </div>) : null }
           </DialogPanel>
         </div>
       </div>
